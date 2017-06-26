713122246b2c7a0ff4786106ef9286efa6d0f7b4
drkwjhnsn <drk.w.jhnsn@gmail.com>
2017-06-26 14:22:46 -0700

    Feat/front end (#4)
    * Implement front-end foundation

--- server/index.js
+++ server/index.js
@@ -2,8 +2,7 @@ var express = require('express');
 
 var app = express();
 
-// UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../client/dist'))
+app.use(express.static(__dirname + '/../client/dist'))
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
