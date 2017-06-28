a78fdbb59b0ed46f7eac16f739592d92336e766a
Kenly Hui <kenlyhui@Kenlys-MacBook-Pro.local>
2017-06-28 10:20:12 -0700

    'june28 morning'

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
-const promise = require('bluebird');
+const Promise = require('bluebird');
 const difference = require('underscore').difference;
 const options = {
-    promiseLib: promise
+    promiseLib: Promise
 };
 const pgp = require('pg-promise')(options);
 
@@ -29,42 +29,66 @@ exports.query = function(queryStr, callback){
 
 };
 
-let getRandomImage = (part) => {
+
+let getImage = (id, part, callback) => {
+
+  db.any(`select _path from ${part} where id = ${id}`)
+    .then(path => {
+      // console.log(path);
+      // return path[0]._path;
+      // console.log('this is consolelog', path[0]._path);
+      callback(path[0]._path);
+    })
+    .catch(err => {
+      console.log(err);
+  });
+
+}
+exports.getRandomImage = (part,callback) => {
+
   db.any(`select id from ${part} order by id desc limit 1`)
     .then(maxId => {
-      return Math.floor(Math.random * (maxID));
+      var id = maxId[0].id;
+      return Math.floor(Math.random() * (id)) + 1;
     })
     .then(id => {
-      return getImage(id, part);
+      // console.log(part);
+      // console.log(id);
+      getImage(id, part, callback);
     })
     .catch(error => {
       console.log(error);
-    });
+  });
+
 };
 
-let getImage = (id, part) => {
-  db.any(`select _path from ${part} where id = ${id}`)
-    .then(path => {
-      return path;
-    })
-    .catch(err => {
-      console.log(error);
-  });
-}
 
-exports.twoImages = function(part, callback) {
+exports.getTwoImages = function(part, callback) {
+
   let arr1 = ['head', 'torso', 'legs'];
-  let diff = _.difference(arr1, [part]);
+  let diff = difference(arr1, [part]);
   let partObj = {};
+
+  console.log(getRandomImage('head'));
 
   // iterate diff 
   diff.forEach((each) => {
     let obj = {};
-    obj['path'] = getRandomImage(each);
-    partObj[each] = obj;
+    getRandomImage(each, (data) => {
+      obj[each] = data;
+      partObj[each] = obj;
+    });
+  })
+  .then((data) => {
+    callback(data);
+  })
+  .catch((err) => {
+    callback(err);
   });
-  return partObj;
-}
+  
+
+};
+// {obj{head: abc_path}, obj{torso: def_path}}
 
 
 
--- server/index.js
+++ server/index.js
@@ -8,17 +8,23 @@ app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// app.get('/testing', (req, res) => {
-//   var str = 'select _path from torso where id = 2';
-//   db.query(str, (data) => {
-//     res.send(data);
-//   });
-// });
-
-app.get('/getTwoImages', (req, res) => {
-  console.log(req.url.split('=')[1] === 'head');
-
+app.get('/testing', (req, res) => {
+  // var str = 'select _path from torso where id = 2';
+  // db.query(str, (data) => {
+  //   res.send(data);
+  // });
+  // db.getRandomImage('head');
+  // console.log(db.getTwoImages('head'));
+  // console.log(db.getImage(1,'head'));
+  db.getRandomImage('head', (data) => {
+    res.send(data);
+  });
 });
+
+// app.get('/getTwoImages', (req, res) => {
+//   console.log(req.url.split('=')[1] === 'head');
+
+// });
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
